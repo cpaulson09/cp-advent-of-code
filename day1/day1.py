640c0b48507f287f7a401d2ca3b7c26bf4b4a74e
Connor Paulson <cpaulson09@hotmail.com>
2021-11-30 23:57:48 -0700

    updates to day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,15 +1,11 @@
 numbers = open('data.txt', 'r').readlines()
 increases = 0
-previous = numbers[0].strip() # assign 1st number to previous
+previous = 0
 for number in numbers:
 	currentNumber = number.strip()
 	print(currentNumber)
 	if currentNumber > previous:
 		print('increased\n')
 		increases += 1
-	elif currentNumber == previous:
-		print('same number\n')
-	else:
-		print('decreased\n')
 	previous = currentNumber
 print(increases)
