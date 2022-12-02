2f9d25497afed6d4e6d4e40eda0e48d0fc51dc5f
Connor Paulson <cpaulson09@hotmail.com>
2022-12-01 19:36:06 -0700

    clean up day 1

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -1,29 +1,17 @@
 with open('data.txt', 'r') as data:
-    first = 0
-    second = 0
-    third = 0
-    current = 0
+    first, second, third, current = 0, 0, 0, 0
     for i in enumerate(data):
         row = i[1].rstrip('\n')
         if row:
             current = current + int(row)
         else:
-            # part 1
             if current > first:
+                third = second # make it 3rd
+                second = first # make it 2nd
                 first = current
-
-            # part 2
-            # if current > first:
-            #     third = second # make it 3rd
-            #     second = first # make it 2nd
-            #     first = current
-            # elif current > second:
-            #     second = current
-            # elif current > third:
-            #     third = current
-
-            current = 0 # reset it blank line, new elf
-    print('most calories: ', first)
-    print('2nd calories: ', second)
-    print('3rd calories: ', third)
+            elif current > second:
+                second = current
+            elif current > third:
+                third = current
+            current = 0 # reset (blank line, new elf)
     print('total: ', first + second + third)
